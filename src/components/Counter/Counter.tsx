42f45481c034c70bb3550f03779e72c06f8dee99
BuTomer <bu.tomer@gmail.com>
2024-03-12 13:58:19 +0200

    lec2

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,10 +1,17 @@
+import { useState } from "react";
+
 function Counter() {
+  const [x, setX] = useState(0);
+
   return (
     <div className="flex flex-col items-center">
-      <button onClick={()=>{}} className="bg-purple-400 p-5 rounded-xl text-white text-2xl">
+      <button
+        onClick={() => setX(x + 1)}
+        className="bg-purple-400 p-5 rounded-xl text-white text-2xl"
+      >
         +
       </button>
-      <p>0</p>
+      <p>{x}</p>
       <button className="bg-purple-400 p-5 rounded-xl text-white text-2xl">
         -
       </button>
