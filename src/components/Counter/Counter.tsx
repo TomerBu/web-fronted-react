b62917ab0e695998f0776ae6677bebb17c4e8d88
BuTomer <bu.tomer@gmail.com>
2024-03-12 13:29:21 +0200

    lec2

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,11 +1,13 @@
 function Counter() {
   return (
     <div className="flex flex-col items-center">
-      <button className="bg-purple-400 p-5 rounded-xl text-white text-2xl">
+      <button onClick={()=>{}} className="bg-purple-400 p-5 rounded-xl text-white text-2xl">
         +
       </button>
       <p>0</p>
-      <button>-</button>
+      <button className="bg-purple-400 p-5 rounded-xl text-white text-2xl">
+        -
+      </button>
     </div>
   );
 }
