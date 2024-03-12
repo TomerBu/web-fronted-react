6070bfb1c7ba0edf2af710a2f7bd8dcd22396a13
BuTomer <bu.tomer@gmail.com>
2024-03-12 11:35:21 +0200

    web-frontend-react

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.scss";
+import Header from "./components/Header/Header";
+ 
+function App() {
+  return (
+    <div>
+      <Header/>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,7 @@
+import "./Header.scss";
+
+function Header() {
+  return <header className="site-header">Header</header>;
+}
+
+export default Header;
