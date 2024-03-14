bfe25b48265365ac81e8436df4e5086a238ee832
BuTomer <bu.tomer@gmail.com>
2024-03-14 17:22:51 +0200

    lec3

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,5 +1,6 @@
 import { FC, InputHTMLAttributes } from "react";
 import "./Input.scss";
+import { CiCircleCheck, CiCircleRemove} from 'react-icons/ci';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
@@ -9,7 +10,9 @@ const Input: FC<InputProps> = ({ label, ...props }) => {
   return (
     <div className="input-container">
       <label>{label}</label>
-      <input {...props} />
+      <input {...props}  />
+      <CiCircleRemove color="red" size={50} />
+      <CiCircleCheck color="green" size={50} />
     </div>
   );
 };
