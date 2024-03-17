f95377bfb4bc649403aadb09511a0c24d7ea44af
BuTomer <bu.tomer@gmail.com>
2024-03-17 10:31:00 +0200

    lec4

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Register from "./routes/Register.tsx";
+import Login from "./routes/Login.tsx";
+import Cards from "./routes/Cards.tsx";
+import App from "./App.tsx";
+import Error from "./routes/Error.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  { path: "/", element: <App />, errorElement: <Error /> },
+  { path: "/register", element: <Register /> },
+  { path: "/login", element: <Login /> },
+  { path: "/cards", element: <Cards /> },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/routes/Error.tsx
@@ -0,0 +1,23 @@
+import { useRouteError } from "react-router-dom";
+
+type ErrorType = {
+  status: number;
+  statusText: string;
+  data: string;
+};
+
+const Error = () => {
+  const { data, status, statusText } = useRouteError() as ErrorType;
+
+  return (
+    <div id="error-page">
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{statusText}</i>
+      </p>
+    </div>
+  );
+};
+
+export default Error;
