3d65667cb4ec4605047e5f489525e528eed07593
BuTomer <bu.tomer@gmail.com>
2024-03-19 11:42:39 +0200

    form-with-state

--- src/routes/Register.tsx
+++ src/routes/Register.tsx
@@ -1,11 +1,43 @@
-import axios from "axios";
+import { useState } from "react";
 
 const Register = () => {
-  // axios = http ספריה שמפשטת עבודה עם 
-  // איך יוצרים טופס ידנית בלי ספריה
-  // בד"כ נשתמש בספריה כדי לפשט את הטופס
+  const [first, setFirst] = useState<string>();
+  const [firstErr, setFirstErr] = useState<string>();
 
-  return <div>Register</div>;
+  const onRegister = () => {
+    if (!firstErr) {
+      alert(`You entered ${first}`);
+    }else{
+      alert('fill the form')
+    }
+  };
+  return (
+    <form>
+      <input
+        onChange={(e) => {
+          const first = e.target.value;
+          if (!first) {
+            setFirstErr("name must not be empty");
+            return;
+          }
+          if (first.length < 2 || first.length > 255) {
+            setFirstErr("name must be between 2-255 characters");
+            return;
+          }
+          setFirst(first);
+          setFirstErr(null);
+        }}
+        type="text"
+        name="first"
+        placeholder="first"
+      />
+      {firstErr && <p className="text-red-600">{firstErr}</p>}
+
+      <button onClick={onRegister} type="button">
+        Register
+      </button>
+    </form>
+  );
 };
 
 export default Register;
