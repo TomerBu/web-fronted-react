3fca5c77fc57ac51ee1159d47d748ffd04e987f2
BuTomer <bu.tomer@gmail.com>
2024-03-19 14:16:08 +0200

    hook-form custom validation

--- src/routes/Register.tsx
+++ src/routes/Register.tsx
@@ -1,76 +1,59 @@
 import { useForm } from "react-hook-form";
 
 const Register = () => {
-  const { register, handleSubmit, control, formState } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   const onRegister = (data) => {
     console.log(data);
   };
 
   return (
-    <form onSubmit={handleSubmit(onRegister)}>
+    <form noValidate onSubmit={handleSubmit(onRegister)}>
       <input
-        required
-        minLength={2}
-        maxLength={10}
+        placeholder="firstname"
         type="text"
-        {...register("firstname")}
+        {...register("firstname", {
+          required: "This field is mandatory",
+          minLength: { value: 2, message: "Too short" },
+          maxLength: { value: 255, message: "Too long" },
+        })}
       />
+      {errors.firstname && (
+        <p className="text-red-500">{errors.firstname.message as string}</p>
+      )}
+
       <input
-        required
-        minLength={2}
-        maxLength={255}
+        placeholder="lastname"
         type="text"
-        {...register("lastname")}
+        {...register("lastname", {
+          required: "This field is mandatory",
+          minLength: { value: 2, message: "Too short" },
+          maxLength: { value: 255, message: "Too long" },
+        })}
       />
+      {errors.lastname && (
+        <p className="text-red-500">{errors.lastname.message as string}</p>
+      )}
+
       <input
-        required
-        min={2}
-        max={130}
+        placeholder="age"
         type="number"
-        {...register("age")}
+        {...register("age", {
+          required: "This field is mandatory",
+          min: { value: 2, message: "You must be at least 2 years old" },
+          max: { value: 130, message: "You can't be older than 130 years old" },
+        })}
       />
+      {errors.age && (
+        <p className="text-red-500">{errors.age.message as string}</p>
+      )}
       <button type="submit">Register</button>
     </form>
   );
 };
 
 export default Register;
-
-/*
- 
-const url = "https://monkfish-app-z9uza.ondigitalocean.app/bcard2/users";
-
-const json = {
-  name: {
-    first: "Ell",
-    middle: "",
-    last: "Vis",
-  },
-  phone: "0512345567",
-  email: "MikeBanana@email.com",
-  password: "Abc!123Abc",
-  image: {
-    url: "",
-    alt: "",
-  },
-  address: {
-    state: "IL",
-    country: "Israel",
-    city: "Arad",
-    street: "Shoham",
-    houseNumber: 5,
-    zip: 8920435,
-  },
-  isBusiness: true,
-};
-
-axios.post(url, json)
-// fetch(url, {
-//   method: "POST",
-//   headers: {
-//     "Content-type": "application/json",
-//   },
-//   body: JSON.stringify(json),
-// });
- */
