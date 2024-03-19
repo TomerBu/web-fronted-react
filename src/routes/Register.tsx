13d52ae9fb94dcbb6ed736321293eb932ebb5f51
BuTomer <bu.tomer@gmail.com>
2024-03-19 11:03:58 +0200

    lec5

--- src/routes/Register.tsx
+++ src/routes/Register.tsx
@@ -1,7 +1,49 @@
-const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+import axios from "axios";
 
-export default Register
+const Register = () => {
+  // axios = http ספריה שמפשטת עבודה עם 
+  // איך יוצרים טופס ידנית בלי ספריה
+  // בד"כ נשתמש בספריה כדי לפשט את הטופס
+
+  return <div>Register</div>;
+};
+
+export default Register;
+
+/*
+ 
+const url = "https://monkfish-app-z9uza.ondigitalocean.app/bcard2/users";
+
+const json = {
+  name: {
+    first: "Ell",
+    middle: "",
+    last: "Vis",
+  },
+  phone: "0512345567",
+  email: "MikeBanana@email.com",
+  password: "Abc!123Abc",
+  image: {
+    url: "",
+    alt: "",
+  },
+  address: {
+    state: "IL",
+    country: "Israel",
+    city: "Arad",
+    street: "Shoham",
+    houseNumber: 5,
+    zip: 8920435,
+  },
+  isBusiness: true,
+};
+
+axios.post(url, json)
+// fetch(url, {
+//   method: "POST",
+//   headers: {
+//     "Content-type": "application/json",
+//   },
+//   body: JSON.stringify(json),
+// });
+ */
