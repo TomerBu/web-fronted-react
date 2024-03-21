a8bc583ee740679a6c7fbf59a3f42512eed08b3f
BuTomer <bu.tomer@gmail.com>
2024-03-21 14:04:18 +0200

    login

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,23 +1,59 @@
 import { useForm } from "react-hook-form";
 import { LoginUser } from "../@types/types";
+import patterns from "../validation/patterns";
+import auth from "../services/auth";
+import dialogs from "../ui/dialogs";
 
 const Login = () => {
-  
   // function that is invoked after successful submission:
-  function onLogin(data: LoginUser) {}
+  const onLogin = (data: LoginUser) => {
+    auth
+      .login(data)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((e) => {
+        dialogs.error("Login Error", e.response.data);
+      });
+  };
 
-  const {register, handleSubmit, formState:{errors}} = useForm<LoginUser>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginUser>();
+
   return (
     <div>
       <h2>Login Page</h2>
-      <form onSubmit={handleSubmit(onLogin)}>
-          {/* email */}
-          <section></section>
+      <form noValidate onSubmit={handleSubmit(onLogin)}>
+        {/* email */}
+        <section>
+          <input
+            placeholder="Email"
+            type="email"
+            {...register("email", {
+              required: "This field is mandatory",
+              pattern: patterns.email,
+            })}
+          />
+          {errors.email && <p>{errors.email?.message}</p>}
+        </section>
 
-          {/* password */}
-          <section></section>
+        {/* password */}
+        <section>
+          <input
+            placeholder="Password"
+            type="password"
+            {...register("password", {
+              required: "This field is mandatory",
+              pattern: patterns.password,
+            })}
+          />
+          {errors.password && <p>{errors.password?.message}</p>}
+        </section>
 
-          <button>Login</button>
+        <button>Login</button>
       </form>
     </div>
   );
--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,13 +1,15 @@
 import axios from "axios";
-import { RegisterUser } from "../@types/types";
+import { LoginUser, RegisterUser } from "../@types/types";
  
 export const baseUrl = "https://monkfish-app-z9uza.ondigitalocean.app/bcard2";
 export const usersUrl = `${baseUrl}/users`;
+export const loginUrl = `${baseUrl}/users/login`;
 
 export const register = (data: RegisterUser) => axios.post(usersUrl, data);
+export const login = (data: LoginUser) => axios.post(loginUrl, data);
 
 export const auth = {
-  register,
+  register, login
 };
 
 export default auth;
