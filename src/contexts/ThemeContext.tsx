f963a3057d3a0b6c27635a8e4df1a88f0032d3e2
BuTomer <bu.tomer@gmail.com>
2024-03-26 11:36:41 +0200

    lec7

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 export const ThemeContext = createContext({
   theme: "light",
@@ -6,20 +6,37 @@ export const ThemeContext = createContext({
 });
 
 export const ThemeProvider = ({ children }) => {
+  useEffect(() => {
+    //read from local storage:
+    let t = localStorage.getItem("theme") ?? "light";
+    // update the state:
+    setTheme(t);
+    // update the class on the body
+    if (t === "dark") {
+      document.body.classList.add("dark");
+    }
+  }, []);
+
   const [theme, setTheme] = useState("light");
 
   // localStorage + useEffect
   // toggle button to change theme
   const toggle = () => {
+    // toggle the theme
     const newValue = theme == "light" ? "dark" : "light";
 
+    // save to local storage
+    localStorage.setItem("theme", newValue);
+
+    // update the class on the body
     if (newValue === "dark") {
       document.body.classList.add("dark");
     } else {
       document.body.classList.remove("dark");
     }
 
-    setTheme((prev) => (prev === "light" ? "dark" : "light"));
+    // update the state
+    setTheme(newValue);
   };
 
   return (
