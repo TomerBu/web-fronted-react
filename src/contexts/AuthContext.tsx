8d5650bc8fd0dd9557b342556688f6fca030256c
BuTomer <bu.tomer@gmail.com>
2024-03-26 13:49:41 +0200

    auth-context

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -2,14 +2,22 @@ import { createContext, useState } from "react";
 
 export const AuthContext = createContext({
   isLoggedIn: false,
-  login: () => {},
+  login: (jwt: string) => {},
   logout: () => {},
 });
 
 export const AuthContextProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const login = () => {};
-  const logout = () => {};
+
+  const login = (jwt: string) => {
+    setIsLoggedIn(true);
+    localStorage.setItem("token", jwt);
+  };
+
+  const logout = () => {
+    setIsLoggedIn(false);
+    localStorage.removeItem("token");
+  };
 
   return (
     <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
