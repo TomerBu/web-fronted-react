8a3faed7b2cf439052b731dfa970c10132b2d0e8
BuTomer <bu.tomer@gmail.com>
2024-04-04 15:12:39 +0300

    loading and error state

--- src/routes/Cards.tsx
+++ src/routes/Cards.tsx
@@ -2,19 +2,38 @@ import { useEffect, useState } from "react";
 import { CardType } from "../@types/types";
 import { getCards } from "../services/cards";
 import { Link } from "react-router-dom";
+import Spinners from "../components/Spinners";
 
 const Cards = () => {
   const [cards, setCards] = useState<CardType[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>();
 
   //SRP:
   useEffect(() => {
-    getCards().then((res) => setCards(res.data));
+    setError(null);
+    setLoading(true);
+    getCards()
+      .then((res) => {
+        setCards(res.data);
+        setError(null);
+      })
+      .catch((e) => {
+        setError("Network error");
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   }, []);
 
   return (
     <div className="flex flex-col justify-center items-center">
+      {loading && <Spinners />}
+      {error && <div>{error}</div>}
+      
       {cards.map((c) => (
-        <Link to={`cards/${c._id}`}
+        <Link
+          to={`cards/${c._id}`}
           key={c._id}
           className="shadow-2xl p-5 w-1/2 mx-auto rounded-md my-2 text-center"
         >
