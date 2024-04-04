904ab0d35bf5cc88c3aac9e35300d076cb338707
BuTomer <bu.tomer@gmail.com>
2024-04-04 10:16:08 +0300

    protected route

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,10 +1,8 @@
-import { useContext } from "react";
-import { AuthContext } from "../contexts/AuthContext";
 import { Navigate } from "react-router-dom";
 import { FCC } from "../@types/types";
 
 const ProtectedRoute: FCC = ({ children }) => {
-  const { isLoggedIn } = useContext(AuthContext);
+  const isLoggedIn = localStorage.getItem("token");
 
   if (!isLoggedIn) {
     return <Navigate to={"/"} replace />;
