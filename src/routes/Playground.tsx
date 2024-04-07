2b93e8a4c48f87db2bd17d23301302eec656803e
BuTomer <bu.tomer@gmail.com>
2024-04-07 15:28:18 +0300

    useReducer

--- src/routes/Playground.tsx
+++ src/routes/Playground.tsx
@@ -1,6 +1,14 @@
-import { Button } from "@mui/material";
+import { Button, Stack } from "@mui/material";
 import { useCounter } from "../hooks/useCounter";
 import { useAuth } from "../hooks/useAuth";
+import {
+  memo,
+  useCallback,
+  useEffect,
+  useMemo,
+  useReducer,
+  useState,
+} from "react";
 
 const Item = ({ text, collapsed, id, callback }) => {
   if (collapsed) {
@@ -18,15 +26,80 @@ const Item = ({ text, collapsed, id, callback }) => {
   return <div>{text}</div>;
 };
 
-const Playground = () => {
+type MyComponentProps = { text: string; callback: () => void };
 
-  const { count, setCount } = useCounter();
-  const { count: count2, setCount: setCount2 } = useCounter();
+const MyComponent = memo(({ text, callback }: MyComponentProps) => {
+  useEffect(() => {
+    console.log("MyComponent was updated");
+  });
+
+  return <div onClick={callback}>{text}</div>;
+});
+
+const Playground = () => {
+  const [count, setCount] = useState(0);
+
   return (
     <>
+      <MyComponent text="hi" callback={useCallback(() => {}, [])} />
       <Button onClick={() => setCount((c) => c + 1)}>{count}</Button>
-      <Button onClick={() => setCount2((c) => c + 1)}>{count2}</Button>
     </>
+  );
+};
+
+const FizzBuzz = () => {
+  const [currentNumber, setCurrentNumber] = useState(0);
+
+  const fizzBuzzResults = useMemo(() => {
+    const results: string[] = [];
+    for (let i = 0; i < 1000; i++) {
+      //divisible by 3 "fizz"
+      //divisible by 5 "buzz"
+      //divisible by 15 "fiz-buzz"
+      //not divisible - ""
+      if (i % 3 == 0 && i % 5 == 0) {
+        results.push("fizz-buzz");
+      } else if (i % 3 == 0) {
+        results.push("fizz");
+      } else if (i % 5 == 0) {
+        results.push("buzz");
+      } else {
+        results.push("");
+      }
+    }
+
+    return results;
+  }, []);
+
+  return (
+    <Stack>
+      {currentNumber}
+      <Button onClick={() => setCurrentNumber((c) => c + 1)}>Next</Button>;
+    </Stack>
+  );
+};
+
+const initialState = 0;
+
+const reducer = (state: number, action: string) => {
+  switch (action) {
+    case "plus":
+      return state + 1;
+    case "minus":
+      return state - 1;
+    case "reset":
+      return initialState;
+    default:
+      return state;
+  }
+};
+
+const Counter = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <Stack>
+      <h2>{state}</h2>
+    </Stack>
   );
 };
 
