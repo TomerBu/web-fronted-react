4d47399c7d54f743757bc888365ac3c8bf9be2f2
BuTomer <bu.tomer@gmail.com>
2024-04-07 10:03:55 +0300

    search in data set

--- src/routes/Playground.tsx
+++ src/routes/Playground.tsx
@@ -1,6 +1,7 @@
 import { Button, Stack, TextField } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { CardType } from "../@types/types";
 
 const Item = ({ text, collapsed, id, callback }) => {
   if (collapsed) {
@@ -20,15 +21,30 @@ const Item = ({ text, collapsed, id, callback }) => {
 
 const Playground = () => {
   const api = `https://monkfish-app-z9uza.ondigitalocean.app/bcard2/cards`;
-  const [allCards, setAllCards] = useState([]);
+  const [allCards, setAllCards] = useState<CardType[]>([]);
+  const [filteredCards, setFilteredCards] = useState<CardType[]>([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     axios.get(api).then((res) => setAllCards(res.data));
   }, []);
 
+  useEffect(() => {
+    const f = allCards.filter((c) => c.title.includes(search));
+    setFilteredCards(f);
+  }, [search]);
+
   return (
     <Stack>
-      {allCards.map((c) => (
+      <TextField
+        onChange={(e) => {
+          setSearch(e.currentTarget.value);
+        }}
+        variant="outlined"
+        label="Search"
+        required
+      />
+      {filteredCards.map((c) => (
         <div key={c._id}>{c.title}</div>
       ))}
     </Stack>
